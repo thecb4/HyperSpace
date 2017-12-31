96880d533eed4593786746ae84047f2ef595eec9
TheCB4 <cavelle@thecb4.io>
2017-12-31 13:09:11 -0500

    removed force dynamic lib

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "HyperSpace",
-            type: .dynamic,
+//            type: .dynamic,
             targets: ["HyperSpace"]),
     ],
     dependencies: [
