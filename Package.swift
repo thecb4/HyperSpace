4247b6fca2cf6c9de00ae71e17eb7204428a0d85
TheCB4 <cavelle@thecb4.io>
2018-03-10 16:44:58 -0500

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-      .package(url: "https://github.com/thecb4/Result.git", .upToNextMinor(from: "0.5.0"))
+      .package(url: "https://github.com/thecb4/Result.git", .upToNextMinor(from: "0.6.0"))
 //      .package(url: "https://github.com/freshOS/then.git",  .upToNextMinor(from: "3.0.2"))
     ],
     targets: [
